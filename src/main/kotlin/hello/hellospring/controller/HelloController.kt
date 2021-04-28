89041e8ec106ef771b725e1eece5db805bcfb275
Seokhyun Bae <bus1029@gmail.com>
2021-04-28 22:02:46 +0900

    feat: API 방식으로 HTTP Body에 데이터를 바로 담을 수 있는 컨트롤러 작성
    - ResponseBody Annotation을 사용해서 Template에 매핑시키지 않고 HTTP 응답
    바디에 바로 데이터를 담을 수 있는 컨트롤러들 작성
    - 이 때 객체가 반환되는 경우 기본적으로 JSON으로 반환

--- src/main/kotlin/hello/hellospring/controller/HelloController.kt
+++ src/main/kotlin/hello/hellospring/controller/HelloController.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HelloController {
@@ -17,5 +18,25 @@ class HelloController {
   fun helloMvc(@RequestParam("name") name: String, model: Model): String {
     model.addAttribute("name", name)
     return "hello-template"
+  }
+
+  @GetMapping("hello-string")
+  // HTTP 통신 프로토콜의 Body에 해당 데이터를 직접 넣어주겠다는 의미
+  @ResponseBody
+  fun helloString(@RequestParam("name") name: String): String {
+    return "hello " + name
+  }
+
+  @GetMapping("hello-api")
+  @ResponseBody
+  fun helloApi(@RequestParam("name") name: String): Hello {
+    val hello = Hello()
+    hello.name = name
+    return hello
+  }
+
+  // Kotlin은 묵시적으로 static class로 생성됨
+  class Hello {
+    var name: String = ""
   }
 }
