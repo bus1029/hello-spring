fbc1106df3430b1f0d88ec25d02010cc0827e1cb
Seokhyun Bae <bus1029@gmail.com>
2021-05-12 19:13:37 +0900

    feat: 회원 홈, 회원 가입, 회원 조회 기능 추가
    - HomeController로 home.html을 출력해 회원 홈페이지를 생성
    - home.html에는 members/new 와 members로 이동시켜주는 버튼이 존재
    - MemberController에서 두 URL에 대한 요청을 받아들일 수 있는 로직 작성
    (GET, POST)
    - 회원 등록을 진행할 경우 createMemberForm으로 이동하게 되는데, 사용자가 이름을 
    입력하게 되면 members/new URL로 POST Request가 발생
    - members/new POST Request에 해당하는 로직을 MemberController에 생성하여 
    이를 받아들인 후 실제 Member객체를 생성
    - 객체를 생성한 후 홈 화면으로 리다이렉트
    - 사용자가 회원 목록을 조회하게 되면 /members로 이동하게 되는데, 이에 해당하는 
    로직이 MemberController에 작성되어 있고, 실제 서비스에서 전체 회원 목록을 받아
    addAttributes로 members라는 변수에 모두 넘김
    - 그러면 실제로 memberList.html이 members라는 변수에서 값들을 꺼내  타임리프
    자체 문법으로 for문을 돌면서 member의 id와 name을 화면에 출력

--- src/main/kotlin/hello/hellospring/controller/MemberController.kt
+++ src/main/kotlin/hello/hellospring/controller/MemberController.kt
@@ -1,10 +1,32 @@
 package hello.hellospring.controller
 
+import hello.hellospring.domain.Member
 import hello.hellospring.service.MemberService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class MemberController @Autowired constructor(private var memberService: MemberService) {
+  @GetMapping("/members/new")
+  fun createForm(): String {
+    return "members/createMemberForm"
+  }
 
+  @PostMapping("/members/new")
+  fun create(form: MemberForm): String {
+    val member = Member(form.name)
+    memberService.join(member)
+
+    return "redirect:/"
+  }
+
+  @GetMapping("/members")
+  fun list(model: Model): String {
+    val members = memberService.findMembers()
+    model.addAttribute("members", members)
+    return "members/memberList"
+  }
 }
---
+++ src/main/kotlin/hello/hellospring/controller/MemberForm.kt
@@ -0,0 +1,5 @@
+package hello.hellospring.controller
+
+class MemberForm {
+  var name: String = ""
+}
