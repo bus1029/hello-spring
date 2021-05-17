f3a8d52b3d2ac3e922ca2122f8403fde3ac9406f
Seokhyun Bae <bus1029@gmail.com>
2021-05-17 21:45:27 +0900

    feat: Jdbc를 사용해 Repository 구현
    - h2 데이터베이스 및 jdbc를 사용하기 위해 gradle에 dependency 추가
    - Jdbc를 사용하는 Repository 클래스 추가
    - SpringConfig에 기존에 사용한 MemoryRepository 대신 jdbcRepository로 변경

--- src/main/kotlin/hello/hellospring/SpringConfig.kt
+++ src/main/kotlin/hello/hellospring/SpringConfig.kt
@@ -1,13 +1,17 @@
 package hello.hellospring
 
+import hello.hellospring.repository.JdbcMemberRepository
 import hello.hellospring.repository.MemberRepository
 import hello.hellospring.repository.MemoryMemberRepository
 import hello.hellospring.service.MemberService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
 
 @Configuration
-class SpringConfig {
+class SpringConfig @Autowired constructor(private val dataSource: DataSource) {
+
   @Bean
   fun memberService(): MemberService {
     return MemberService(memberRepository())
@@ -15,6 +19,6 @@ class SpringConfig {
 
   @Bean
   fun memberRepository(): MemberRepository {
-    return MemoryMemberRepository()
+    return JdbcMemberRepository(dataSource)
   }
 }
---
+++ src/main/kotlin/hello/hellospring/repository/JdbcMemberRepository.kt
@@ -0,0 +1,140 @@
+package hello.hellospring.repository
+
+import hello.hellospring.domain.Member
+import org.springframework.jdbc.datasource.DataSourceUtils
+import java.sql.*
+import javax.sql.DataSource
+
+
+class JdbcMemberRepository(private val dataSource: DataSource): MemberRepository {
+
+  override fun save(member: Member): Member {
+    val sql = "insert into member(name) values(?)"
+    val con: Connection? = getConnection()
+    val pstmt = con!!.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+    var rs: ResultSet? = null
+    try {
+      pstmt.setString(1, member.name)
+      pstmt.executeUpdate()
+      rs = pstmt.generatedKeys
+
+      if (rs.next()) {
+        member.id = rs.getLong(1)
+      } else {
+        throw SQLException("id 조회 실패")
+      }
+
+      return member
+    } catch (e: SQLException) {
+      throw IllegalStateException(e)
+    } finally {
+      close(con, pstmt, rs)
+    }
+
+  }
+
+  override fun findById(id: Long): Member? {
+    val sql = "select * from member where id = ?"
+    var conn: Connection? = null
+    var pstmt: PreparedStatement? = null
+    var rs: ResultSet? = null
+    try {
+      conn = getConnection()
+      pstmt = conn!!.prepareStatement(sql)
+      pstmt.setLong(1, id)
+      rs = pstmt.executeQuery()
+      return if (rs.next()) {
+        val member = Member(rs.getString("name"))
+        member.id = rs.getLong("id")
+        member
+      } else {
+        null
+      }
+    } catch (e: Exception) {
+      throw IllegalStateException(e)
+    } finally {
+      close(conn, pstmt, rs)
+    }
+  }
+
+  override fun findByName(name: String): Member? {
+    val sql = "select * from member where name = ?"
+    var conn: Connection? = null
+    var pstmt: PreparedStatement? = null
+    var rs: ResultSet? = null
+
+    return try {
+      conn = getConnection()
+      pstmt = conn!!.prepareStatement(sql)
+      pstmt.setString(1, name)
+      rs = pstmt.executeQuery()
+      if (rs.next()) {
+        val member = Member(rs.getString("name"))
+        member.id = rs.getLong("id")
+        return member
+      }
+      null
+    } catch (e: java.lang.Exception) {
+      throw IllegalStateException(e)
+    } finally {
+      close(conn, pstmt, rs)
+    }
+  }
+
+  override fun findAll(): List<Member> {
+    val sql = "select * from member"
+    var conn: Connection? = null
+    var pstmt: PreparedStatement? = null
+    var rs: ResultSet? = null
+
+    return try {
+      conn = getConnection()
+      pstmt = conn!!.prepareStatement(sql)
+      rs = pstmt.executeQuery()
+      val members: MutableList<Member> = ArrayList()
+      while (rs.next()) {
+        val member = Member(rs.getString("name"))
+        member.id = rs.getLong("id")
+        members.add(member)
+      }
+      members
+    } catch (e: java.lang.Exception) {
+      throw IllegalStateException(e)
+    } finally {
+      close(conn, pstmt, rs)
+    }
+  }
+
+  private fun getConnection(): Connection? {
+    return DataSourceUtils.getConnection(dataSource)
+  }
+
+  private fun close(conn: Connection?, pstmt: PreparedStatement?, rs: ResultSet?) {
+    try {
+      rs?.let {
+        rs.close()
+      }
+    } catch (e: SQLException) {
+      e.printStackTrace()
+    }
+    try {
+      pstmt?.let {
+        pstmt.close()
+      }
+    } catch (e: SQLException) {
+      e.printStackTrace()
+    }
+    try {
+      conn?.let {
+        close(conn)
+      }
+    } catch (e: SQLException) {
+      e.printStackTrace()
+    }
+  }
+
+  @Throws(SQLException::class)
+  private fun close(conn: Connection) {
+    DataSourceUtils.releaseConnection(conn, dataSource)
+  }
+}
